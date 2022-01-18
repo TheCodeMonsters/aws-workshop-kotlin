7000b5c9ca57043eb72ec533b7a757f40dff1cd6
manuelduarte077 <carlosd99m@gmail.com>
2022-01-18 15:20:19 -0600

    project setup

---
+++ app/src/main/java/com/manuelduarte077/loginrucko/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.manuelduarte077.loginrucko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
